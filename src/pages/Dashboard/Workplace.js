b4bb5f826586effa72df11e743f3dff476a562e7
smallchill <smallchill@163.com>
2025-10-13 11:24:48 +0800

    :tada: 4.7.0.RELEASE 新增i18n国际化组件，新增顶部菜单模块

--- src/pages/Dashboard/Workplace.js
+++ src/pages/Dashboard/Workplace.js
@@ -20,7 +20,7 @@ class Workplace extends PureComponent {
           <Row gutter={24}>
             <Col span={24}>
               <div style={{ textAlign: 'center' }}>
-                <img src="https://img.shields.io/badge/Release-V4.6.0-green.svg" alt="Downloads" />
+                <img src="https://img.shields.io/badge/Release-V4.7.0-green.svg" alt="Downloads" />
                 <img src="https://img.shields.io/badge/JDK-17+-green.svg" alt="Build Status" />
                 <img
                   src="https://img.shields.io/badge/Spring%20Cloud-2025-blue.svg"
@@ -212,7 +212,16 @@ class Workplace extends PureComponent {
             </Row>
             <Row gutter={24}>
               <Card className={styles.card} bordered={false}>
-                <Collapse bordered={false} defaultActiveKey={['42']}>
+                <Collapse bordered={false} defaultActiveKey={['43']}>
+                  <Panel header="4.7.0发布 升级适配 SpringCloud2025 SpringBoot3.5" key="43">
+                    <div>1.[升级]Spring 至 6.2.11</div>
+                    <div>2.[升级]SpringBoot 至 3.5.6</div>
+                    <div>3.[升级]Nacos 至 3.1.0</div>
+                    <div>4.[升级]MybatisPlus 至 3.5.19</div>
+                    <div>5.[新增]I18n国际化组件</div>
+                    <div>6.[新增]顶部菜单模块</div>
+                    <div>7.[新增]BaseService常用基础方法</div>
+                  </Panel>
                   <Panel header="4.6.0发布 升级适配 SpringCloud2025 SpringBoot3.5" key="42">
                     <div>1.[升级]Spring 至 6.2.8</div>
                     <div>2.[升级]SpringBoot 至 3.5.3</div>
